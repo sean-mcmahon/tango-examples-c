5ed2d4d2366772e8ada2d3fe357a8e0b9bfe43a8
Sean McMahon <s1.mcmahon@hdr.qut.edu.au>
2016-06-28 16:43:23 +1000

    saved a double cannot save images yet

--- cpp_rgb_depth_sync_example/app/src/main/jni/rgb_depth_sync_application.cc
+++ cpp_rgb_depth_sync_example/app/src/main/jni/rgb_depth_sync_application.cc
@@ -331,7 +331,7 @@ void SynchronizationApplication::Render() {
 
             std::ofstream myfile;
             myfile.open("/sdcard/Download/Example.bin");
-            myfile.write(reinterpret_cast<const char*>(&color_image_buffer_->data), std::streamsize(image_width_*image_height_*image_depth_));
+            //myfile.write(reinterpret_cast<const char*>(&color_image_buffer_->data), std::streamsize(image_width_*image_height_*image_depth_));
             myfile.write(reinterpret_cast<const char*>(&color_image_buffer_->timestamp), sizeof(double));
             myfile.close();
             saving_to_file_=false;
