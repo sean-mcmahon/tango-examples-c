131df7e5c9f901b867ee9029bd619acafae31545
Sean McMahon <s1.mcmahon@hdr.qut.edu.au>
2016-06-30 12:08:38 +1000

    worried that the colortimestamp is not the same as the colorbuffers timestamp. In other words the image in the buffer is not the same as the image from the timestamp

--- cpp_rgb_depth_sync_example/app/src/main/jni/rgb_depth_sync_application.cc
+++ cpp_rgb_depth_sync_example/app/src/main/jni/rgb_depth_sync_application.cc
@@ -271,6 +271,9 @@ void SynchronizationApplication::Render() {
 
   // In the following code, we define t0 as the depth timestamp and t1 as the
   // color camera timestamp.
+    if (color_image_buffer_==nullptr) {
+        LOGE("color image buffer is a null pointer!");
+    }
 
   // Calculate the relative pose from color camera frame at timestamp
   // color_timestamp t1 and depth
@@ -336,7 +339,7 @@ void SynchronizationApplication::Render() {
             LOGI("Saved example file, timestamp: %f, sizeof: %zu, image size %u ", color_timestamp,(sizeof(color_image_buffer_->data)/sizeof(*color_image_buffer_->data)), std::streamsize(image_width_*image_height_*image_depth_));
             LOGI("ColorCameraIntinsics. height: %d, width: %d, depth: %d, and uint8_t size: %zu",image_height_, image_width_, image_depth_,
                  sizeof(uint8_t) );
-            LOGI("ColorImageBuffer. height: %d, width: %d, depth: %d, image_length %d, buffer timestamp %f, and uint8_t size: %zu",color_image_buffer_->height, color_image_buffer_->width, image_depth_,
+            LOGI("ColorImageBuffer. height: %d, width: %d, depth: %d, image_length %d, buffer timestamp %f, and uint8_t size: %zu",render_buffer_->color_image->height, color_image_buffer_->width, image_depth_,
                  color_image_buffer_->height*color_image_buffer_->width,color_image_buffer_->timestamp ,sizeof(uint8_t) );
             LOGI("First few values of color_image_buffer_->data are: %u, %u, %u, %u, %u ",color_image_buffer_->data[0],color_image_buffer_->data[1],color_image_buffer_->data[2],color_image_buffer_->data[3],color_image_buffer_->data[4] );
         }
