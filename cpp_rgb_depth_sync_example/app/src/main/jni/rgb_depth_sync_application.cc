853bac815be7d38247a73c12bbfc73422d632b1b
Sean McMahon <s1.mcmahon@hdr.qut.edu.au>
2016-07-04 08:41:58 +1000

    YUV images have array size of 1.5*height x width. Changed the write size to this and got correct read on local cpp code

--- cpp_rgb_depth_sync_example/app/src/main/jni/rgb_depth_sync_application.cc
+++ cpp_rgb_depth_sync_example/app/src/main/jni/rgb_depth_sync_application.cc
@@ -345,7 +345,7 @@ void SynchronizationApplication::Render() {
 
             uint8_t testUint8 = 128;
             std::vector<float> my_depth_image_buffer_ = depth_image_.getDepthMapBuffer();
-            int num_of_pixels_ = sizeof(color_image_buffer_->data) / sizeof(color_image_buffer_->data[0]);
+            long unsigned int num_of_pixels_ = sizeof(color_image_buffer_->data); // / sizeof(color_image_buffer_->data[0]);
             if (my_depth_image_buffer_.empty()) {
                 LOGE("SynchronizationApplication::Render - Depth image buffer empty!");
             }
@@ -353,18 +353,18 @@ void SynchronizationApplication::Render() {
             myfile.open("/sdcard/Download/color_image.bin");
             //myfile.write(reinterpret_cast<const char*>(&testUint8), sizeof(uint8_t));
             //myfile.write(reinterpret_cast<const char*>(&color_image_buffer_->data[1]), sizeof(uint8_t));
-            myfile.write(reinterpret_cast<const char*>(&color_image_buffer_->data[0]), std::streamsize(image_width_*image_height_));
+            myfile.write(reinterpret_cast<const char*>(&color_image_buffer_->data[0]), std::streamsize(image_width_*(image_height_+image_height_/2))); // YUV 420 SP format, sizes are height*1.5, width
 //            myfile.write(reinterpret_cast<const char*>(&color_timestamp), sizeof(double));
 //            myfile.write((char*)&(my_depth_image_buffer_[0]),my_depth_image_buffer_.size() *
 //                    sizeof(float));
             myfile.close();
             //saving_to_file_=false;
-            LOGI("Saved example file, timestamp: %f, sizeof: %zu, image size %d, num of pixels: %d ", render_buffer_->timestamp,sizeof(float), std::streamsize(image_width_*image_height_),num_of_pixels_);
+            LOGI("Saved example file, timestamp: %f, sizeof: %zu, image size %d, num of pixels: %lu ", render_buffer_->timestamp,sizeof(float), std::streamsize(image_width_*(image_height_+image_height_/2))),num_of_pixels_);
 //            LOGI("ColorCameraIntinsics. height: %d, width: %d, depth: %d, and uint8_t size: %zu",image_height_, image_width_, image_depth_,
 //                 sizeof(uint8_t) );
-            LOGI("ColorImageBuffer. height: %d, width: %d, depth: %d,buffer timestamp %f, and uint8_t size: %zu. ",color_image_buffer_->width, color_image_buffer_->height, image_depth_,
-                 color_image_buffer_->timestamp ,sizeof(uint8_t));
-            LOGI("First few values of color_image_buffer_->data are: %u, %u, %u, %u, %u ",color_image_buffer_->data[0],color_image_buffer_->data[220395],color_image_buffer_->data[220405],color_image_buffer_->data[220400],color_image_buffer_->data[230400] );
+            LOGI("ColorImageBuffer. height: %d, width: %d, depth: %d,buffer timestamp %f, and Format: %04x (0x11 = YCbCr_420_SP)",color_image_buffer_->width, color_image_buffer_->height, image_depth_,
+                 color_image_buffer_->timestamp ,color_image_buffer_->format);
+            LOGI("First few values of color_image_buffer_->data are: %u, %u, %u, %u, %u ",color_image_buffer_->data[0],color_image_buffer_->data[220395],color_image_buffer_->data[220405],color_image_buffer_->data[220400],color_image_buffer_->data[230400]);//230400] );
 //            LOGI("First some values of depth_image_buffer are: %f,%f,%f,%f,%f ",my_depth_image_buffer_[50],my_depth_image_buffer_[220395],my_depth_image_buffer_[220405],my_depth_image_buffer_[220400],my_depth_image_buffer_[230400] );
 
         }
