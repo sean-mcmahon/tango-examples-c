b6daea5157c5cc4b4716ebfcdf8db689f1550dc2
Sean McMahon <s1.mcmahon@hdr.qut.edu.au>
2016-07-07 10:14:21 +1000

    seems to work now with integers, the ordering is right the elements being storage appear correct, will go back to image buffer types

--- cpp_rgb_depth_sync_example/app/src/main/jni/rgb-depth-sync/rgb_depth_sync_application.h
+++ cpp_rgb_depth_sync_example/app/src/main/jni/rgb-depth-sync/rgb_depth_sync_application.h
@@ -159,8 +159,9 @@ class SynchronizationApplication {
   // point cloud data which should be rendered.
   TangoXYZij* render_buffer_;
   TangoImageBuffer* color_image_buffer_;
-  std::list<TangoImageBuffer> color_buffer_list_;
-  std::list<TangoImageBuffer>::iterator image_list_iterator_;
+  std::list<int> color_buffer_list_;
+  std::list<int>::iterator image_list_iterator_;
+  int testListInteger = 0;
 
  // Image Sizes
  int image_width_;
